f5a8c18b04dda18a4a0a4139e27911ad36e0d83c
s1270146 <s1270146@gmail.com>
2023-09-30 11:56:15 +0900

    Appbarの色変更

--- lib/component/app_bar/customized_app_bar_theme.dart
+++ lib/component/app_bar/customized_app_bar_theme.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CustomizedAppBarTheme extends AppBarTheme {
-  const CustomizedAppBarTheme()
-      : super(
-          backgroundColor: Colors.purple,
-          titleTextStyle: const TextStyle(
-            color: Colors.white,
+  CustomizedAppBarTheme({
+    Color iconColor = Colors.white,
+    Color textColor = Colors.white,
+    Color backgroundColor = const Color.fromARGB(255, 21, 0, 80),
+  }) : super(
+          backgroundColor: backgroundColor,
+          iconTheme: IconThemeData(
+            color: iconColor,
+          ),
+          titleTextStyle: TextStyle(
+            color: textColor,
             fontSize: 20,
           ),
         );
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-        appBarTheme: const CustomizedAppBarTheme(),
+        appBarTheme: CustomizedAppBarTheme(),
       ),
       home: LoginScreen(),
     );
